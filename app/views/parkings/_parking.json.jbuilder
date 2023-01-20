0b451725ce5e0751294ed65aaaa70ed06cf99f56
naeem asif <naeemgoraya123@gmail.com>
2023-01-20 21:31:10 +0400

    Devise setup + Parking model done

---
+++ app/views/parkings/_parking.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! parking, :id, :name, :description, :created_at, :updated_at
+json.url parking_url(parking, format: :json)
---
+++ db/migrate/20230119201900_create_parkings.rb
@@ -0,0 +1,10 @@
+class CreateParkings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :parkings do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
